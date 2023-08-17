20df7b058f658c042f7bc12683395227fcdaa839
Daniel da Silva <daniel.dasilva@avetta.com>
2023-08-17 10:24:02 -0300

    save state

---
+++ .savedStates/exercise_001_working_with_sources/src/main/scala/com/lightbend/akkassembly/EngineShop.scala
@@ -0,0 +1,14 @@
+package com.lightbend.akkassembly
+
+import akka.NotUsed
+import akka.stream.scaladsl.Source
+
+class EngineShop(shipmentSize: Int) {
+  val shipments: Source[Shipment, NotUsed] = {
+    Source.fromIterator( () => Iterator.continually(
+      Shipment(
+        Seq.fill(shipmentSize)(Engine())
+      )
+    ))
+  }
+}

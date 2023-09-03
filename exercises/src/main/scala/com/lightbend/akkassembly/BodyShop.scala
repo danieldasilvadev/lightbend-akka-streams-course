df42ff0196cbee0ef89c6c11e7c6c5b65107f769
Daniel da Silva <daniel.dasilva@avetta.com>
2023-09-03 15:06:45 -0300

    fusion done

--- exercises/src/main/scala/com/lightbend/akkassembly/BodyShop.scala
+++ exercises/src/main/scala/com/lightbend/akkassembly/BodyShop.scala
@@ -6,5 +6,6 @@ import akka.stream.scaladsl.Source
 import scala.concurrent.duration.FiniteDuration
 
 class BodyShop(buildTime: FiniteDuration) {
-  val cars: Source[UnfinishedCar, Cancellable] = Source.tick(buildTime, buildTime, UnfinishedCar())
+//  val cars: Source[UnfinishedCar, Cancellable] = Source.tick(buildTime, buildTime, UnfinishedCar())
+  val cars = Source.repeat(UnfinishedCar()).throttle(1, buildTime)
 }
